6b36c92cbf597767706f460fae8f00c637d878f1
Alexander Serkov <sana3696g@gmail.com>
2024-01-27 00:12:39 +0300

    Code refactoring

--- lab01/TestExp.cpp
+++ lab01/TestExp.cpp
@@ -1,29 +1,63 @@
-#include "Exp.hpp"
+#include <cassert>
 #include <cmath>
 #include <iostream>
 
-void test_float(float x) {
-    float EXP = ADAAI::Exp(x);
-    float exp = std::exp(x);
-    std::cout << EXP << ' ' << exp << ' ' << std::abs(EXP - exp) << '\n';
+#include "Exp.hpp"
+
+template<typename T, T MimicFunction( T ) = ADAAI::Exp, T RealFunction( T ) = std::exp>
+bool test( T x )
+{
+  T got      = MimicFunction( x );
+  T expected = RealFunction( x );
+  T diff     = std::abs( got - expected );
+
+  std::cout << diff << " = |" << expected << " - " << got << "| (diff) = |(expected) - (got)|\n";
+
+  if ( std::isnan( got ) )
+  {
+    return std::isnan( expected );
+  }
+
+  if ( std::isinf( got ) )
+  {
+    return std::isinf( expected );
+  }
+
+  return diff < 1e-6;
 }
 
-void test_double(double x) {
-    double EXP = ADAAI::Exp(x);
-    double exp = std::exp(x);
-    std::cout << EXP << ' ' << exp << ' ' << std::abs(EXP - exp) << '\n';
+bool check_exp()
+{
+  long double test_numbers[] = {
+      1.0,
+      1345442354523432.0,
+      -1345442354523432.0,
+      0.0,
+      -0.0,
+      10.1,
+      -10.1,
+      100.0,
+      -100.0,
+      0.0000001,
+      -0.0000001,
+      656.0,
+      -656.0,
+  };
+
+  for ( auto x : test_numbers )
+  {
+    assert( ( test<float, ADAAI::Exp, std::exp>( x ) ) );
+    assert( ( test<double, ADAAI::Exp, std::exp>( x ) ) );
+    assert( ( test<long double, ADAAI::Exp, std::exp>( x ) ) );
+  }
+
+  return true;
 }
 
-void test_ldouble(long double x) {
-    long double EXP = ADAAI::Exp(x);
-    long double exp = std::exp(x);
-    std::cout << EXP << ' ' << exp << ' ' << std::abs(EXP - exp) << '\n';
-}
+int main()
+{
+  assert( check_exp() );
 
-int main() {
-    test_float(1.0);
-    test_double(1.0);
-    test_ldouble(1345442354523432);
-    // std::cout << ADAAI::Exp(x4) << '\n'; // this will fail
-    return 0;
+  // std::cout << ADAAI::Exp(x4) << '\n'; // this will fail
+  return 0;
 }
