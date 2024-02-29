da68ae0e10afdebe41e2770655471f0e1a00308c
Daniil <77348708+smartydan@users.noreply.github.com>
2024-02-29 10:42:56 +0300

    Fix typo

--- diff/TestDiff.hpp
+++ diff/TestDiff.hpp
@@ -14,7 +14,7 @@ void TestDiff()
   std::cout << "Derivative dx using Stencil5 of sin( e^x + y^2 ) at point " << x << " " << y << " is " << res << "\n";
   res = Differentiator<Method::Stencil5Extra, D::X>( ExampleFunction, x, y ); // better!
   std::cout << "Derivative dx using extrapolation on top of Stencil5 of sin( e^x + y^2 ) at point " << x << " " << y << " is " << res << "\n";
-  // in reality, it is e^x * cos(e^x + 2y) ≈ −12.3968826882
+  // in reality, it is e^x * cos(e^x + y^2) ≈ −12.3968826882
 
   res = Differentiator<Method::Stencil3, D::XY>( ExampleFunction, x, y );
   std::cout << "Derivative dxdy using 3-points of sin( e^x + y^2 ) at point " << x << " " << y << " is " << res << "\n";
