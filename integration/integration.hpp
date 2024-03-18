47f193915944ecb31cb4d722653e943e0abe3009
Alexander Serkov <32331578+setday@users.noreply.github.com>
2024-03-19 02:00:16 +0300

    Code refactoring + added CD calc

--- integration/integration.hpp
+++ integration/integration.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cmath>
+#include <stdexcept>
 #include <type_traits>
 
 namespace ADAAI::Integration
@@ -8,118 +9,145 @@ namespace ADAAI::Integration
   /// \brief A callable object that returns the value of \rho(h) at the given point h
   struct AirDensity
   {
-    AirDensity( float h )
-        : h( h )
+    struct Layer
+    {
+      float max_height;
+      float min_height;
+
+      float avg_temperature;
+      float avg_pressure;
+      float avg_r;
+    };
+
+  private:
+    float h;
+
+    const float G_force = 9.80655f;  // gravitational acceleration constant
+    const float R_air   = 287.0528f; // R_air constant
+
+    const Layer layers[4] = {
+        // max_height, min_height, avg_temperature, avg_pressure, avg_r
+        { 11'000, 0, 288.15f, 101325.0f, 0.0065f },
+        { 20'000, 11'000, 216.65f, 22632.3777602f, 0 },
+        { 32'000, 20'000, 216.65f, 5475.03644646f, -0.001f },
+        { 47'000, 32'000, 228.65f, 868.056901172f, -0.0028f } // should this have a maximum height?
+    };
+
+  public:
+    explicit AirDensity( float height )
+        : h( height )
     {
     }
 
-    const float g     = 9.80655f;  // gravitational acceleration constant
-    const float R_air = 287.0528f; // R_air constant
-
-    const int h0 = 0;
-    const int h1 = 11'000;
-    const int h2 = 20'000;
-    const int h3 = 32'000;
-    const int h4 = 47'000; // but we won't use this number?
-
-    const float r0 = 0.0065f;
-    const float r1 = 0;
-    const float r2 = -0.001f;
-    const float r3 = -0.0028f;
-
-    const float T0 = 288.15f;
-    const float T1 = 216.65f;
-    const float T2 = 216.65f;
-    const float T3 = 228.65f;
-
-    const float p0 = 101325.0f;
-    const float p1 = 22632.3777602f;
-    const float p2 = 5475.03644646f;
-    const float p3 = 868.056901172f;
-
-    float atmosphericPressure( float h, int caseNum ) const
+    [[nodiscard]] float atmosphericPressure( float height, int caseNum ) const
     {
-      if ( caseNum == 1 )
+      if ( caseNum < 0 || caseNum > 3 )
       {
-        return p0 * std::exp( -g * ( h - h1 ) / ( R_air * T1 ) );
+        throw std::invalid_argument( "atmosphericPressure: Invalid case number" );
       }
-      else
+
+      float
+          p_l = layers[caseNum].avg_pressure,
+          r_l = layers[caseNum].avg_r,
+          h_l = layers[caseNum].min_height,
+          T_l = layers[caseNum].avg_temperature;
+
+      float
+          d_height = height - h_l;
+
+      if ( caseNum == 0 )
       {
-        float r_l, h_l, T_l;
-        if ( caseNum == 0 )
-        {
-          r_l = r0;
-          h_l = h0;
-          T_l = T0;
-        }
-        else if ( caseNum == 2 )
-        {
-          r_l = r2;
-          h_l = h2;
-          T_l = T2;
-        }
-        else // caseNum == 3
-        {
-          r_l = r3;
-          h_l = h3;
-          T_l = T3;
-        }
-        return p0 * std::exp( -g / ( R_air * r_l ) * std::log( 1 - r_l * ( h - h_l ) / T_l ) );
+        return p_l * std::exp( -G_force * d_height / ( R_air * T_l ) );
       }
+
+      return p_l * std::exp( G_force / ( R_air * r_l ) * std::log( 1 - r_l * d_height / T_l ) );
     }
 
     float operator()() const
     {
-      if ( h >= h0 && h <= h1 )
+      for ( int l_id = 0; l_id < 4; l_id++ )
       {
-        return atmosphericPressure( h, 0 ) / ( R_air * ( T0 - r0 * ( h - h0 ) ) );
-      }
-      else if ( h > h1 && h <= h2 )
-      {
-        return atmosphericPressure( h, 1 ) / ( R_air * ( T1 - r0 * ( h - h1 ) ) );
-      }
-      else if ( h > h2 && h <= h3 )
-      {
-        return atmosphericPressure( h, 2 ) / ( R_air * ( T2 - r2 * ( h - h2 ) ) );
-      }
-      else if ( h > h3 && h <= h4 )
-      {
-        return atmosphericPressure( h, 3 ) / ( R_air * ( T3 - r3 * ( h - h3 ) ) );
-      }
-      return static_cast<float>( -239 ); // error
-    }
+        const Layer& layer = layers[l_id];
 
-  private:
-    float h;
+        if ( h < layer.min_height || h > layer.max_height )
+        {
+          continue;
+        }
+
+        return atmosphericPressure( h, l_id ) /
+               ( R_air * ( layer.avg_temperature - layer.avg_r * ( h - layer.min_height ) ) );
+      }
+
+      throw std::invalid_argument( "AirDensity: Invalid height" );
+    }
   };
 
   /// \brief A callable object that returns the value of C_D( M ) at the given point M
 
   struct DrugCoefficient
   {
-    DrugCoefficient( float M )
-        : M( M )
-    {
-    }
-    float CD[60]; // is must be filled with values obtained from a graph
-                         // (check tg message from Merkin)
+  private:
+    float M;
 
-    float slope[60]; // (CD[i + 1] - CD[i]) / (M[i + 1] - M[i])
-                            // must be precomputed
+    float CD[60] {}; // is must be filled with values obtained from a graph
+                     // (check tg message from Merkin)
+    float d_CD = 0.03f;
+
+    float slope[60] {}; // (CD[i + 1] - CD[i]) / (M[i + 1] - M[i])
+                        // must be precomputed
+
+    /// \brief A strange function that is used to compute CD
+    /// \file See ./../assets/Prec.png
+
+    [[nodiscard]] static float g_strange( float x )
+    {
+      return 1.0f / ( x + 1.1f ) - 0.04f;
+    }
+
+    [[nodiscard]] static float f_strange( float x )
+    {
+      return std::pow( 1.0f / ( 2.05f - x ), 20.0f ) + 0.1f;
+    }
+
+    [[nodiscard]] static float h_strange( float x )
+    {
+      if ( x < 0.3f || x > 2.3f )
+      {
+        throw std::invalid_argument( "h_strange: Invalid argument" );
+      }
+
+      return std::min( g_strange( x ), f_strange( x ) );
+    }
+
+    void initialize_CD()
+    {
+      for ( int i = 0; i < 60; i++ )
+      {
+        CD[i] = h_strange( d_CD * ( float ) i + 0.4f );
+      }
+    }
 
     void initialize_slope()
     {
-      // TODO;
+      for ( int i = 0; i < 59; i++ )
+      {
+        slope[i] = ( CD[i + 1] - CD[i] ) / d_CD;
+      }
+    }
+
+  public:
+    explicit DrugCoefficient( float MachNumber )
+        : M( MachNumber )
+    {
+      initialize_CD();
+      initialize_slope();
     }
 
     float operator()() const // M is a Mach number
     {
-      int i = M / 0.05;
-      return CD[i] + slope[i] * ( M - 0.05 * i );
+      int i = ( int ) ( M / d_CD );
+      return CD[i] + slope[i] * ( M - d_CD * ( float ) i );
     }
-
-  private:
-    float M;
   };
 
   /// \brief Computes the value of Q (given M, y and v_x, v_y)
