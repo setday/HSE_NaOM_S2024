5b12855beac4153fb55f925a6700cdcb7dc37a39
Alexander Serkov <32331578+setday@users.noreply.github.com>
2024-04-15 21:24:21 +0300

    Add accel

--- assets/CBPlotCreator.py
+++ assets/CBPlotCreator.py
@@ -16,11 +16,11 @@ print(df.info())
 df['v'] = np.sqrt(df['current_state.v_x']**2 + df['current_state.v_y']**2)
 
 # Plot the data
-figure, axes = plt.subplots(2, 2, figsize=(12, 6))
+figure, axes = plt.subplots(3, 2, figsize=(18, 9))
 
 palette = sns.color_palette("mako_r", 6)
 
-fig = sns.lineplot(data=df, x='current_state.x', y='current_state.y', ax=axes[0, 0], palette=palette)
+fig = sns.lineplot(data=df, x='current_state.x', y='current_state.y', ax=axes[0, 0])
 fig.set_title(f'Cannonball trajectory (angle = {data["angle"]}°)')
 fig.set(xlabel='Distance (m)')
 fig.set(ylabel='Height (m)')
@@ -43,6 +43,18 @@ fig = sns.lineplot(data=df, x='current_state.x', y='current_state.v_y', ax=axes[
 fig.set_title(f'Cannonball speed')
 fig.set(xlabel='Distance (m)')
 fig.set(ylabel='Speed y (m/s)')
+fig.grid(True)
+
+fig = sns.lineplot(data=df, x='current_state.x', y='rhs.a_x', ax=axes[2, 0])
+fig.set_title(f'Cannonball acceleration')
+fig.set(xlabel='Distance (m)')
+fig.set(ylabel='Acceleration x (m/s²)')
+fig.grid(True)
+
+fig = sns.lineplot(data=df, x='current_state.x', y='rhs.a_y', ax=axes[2, 1])
+fig.set_title(f'Cannonball acceleration')
+fig.set(xlabel='Distance (m)')
+fig.set(ylabel='Acceleration y (m/s²)')
 fig.grid(True)
 
 plt.tight_layout()
